9155199bbba5de5cd5916f46c672dbfa6c454389
Cody <codylau6233@hotmail.com>
2024-03-28 18:00:57 -0700

    Add new stores. Implemented where clause to filter rows in stores table.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -5,5 +5,18 @@ require_relative './exercise_3'
 
 puts "Exercise 4"
 puts "----------"
+store1 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store2 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false) 
+store2 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-# Your code goes here ...
+@mens_store = Store.where(mens_apparel: true)
+@mens_store.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+puts "---------------------------------"
+@womens_store = Store.where(womens_apparel: true).where("annual_revenue < ? ", 1000000) 
+@womens_store.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
