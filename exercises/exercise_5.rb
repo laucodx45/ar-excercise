73ca88d5bcf61b39b8eeae1a9f9864a694b7b462
Cody <codylau6233@hotmail.com>
2024-03-29 11:45:58 -0700

    Implemented average, where and count clause

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+pp total_revenue
+
+avg_revenue = Store.average(:annual_revenue)
+pp avg_revenue
+
+# Output the number of stores that are generating $1M or more in annual sales
+num_of_store = Store.where("annual_revenue > ?", 1000000).count
+pp num_of_store
